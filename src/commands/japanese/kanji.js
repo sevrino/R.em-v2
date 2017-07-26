2d8b1feee72ee778e90d56b69e65d920e089715e
Hamza Haiken <hamza.haiken@ordina.nl>
2017-07-26 16:57:12 +0200

    Make kanji command upload correct animations

--- src/commands/japanese/kanji.js
+++ src/commands/japanese/kanji.js
@@ -1,6 +1,9 @@
 let Command = require('../../structures/command');
 
-let fs = require("pn/fs");
+let pnfs = require("pn/fs");
+let fs = require("fs");
+
+var files = fs.readdirSync("../kanji/");
 
 class Kanji extends Command {
     constructor({t}) {
@@ -13,10 +16,24 @@ class Kanji extends Command {
     }
 
     run(msg) {
-        fs.readFile("../kanji/05929_anim.gif")
-            .then(buffer => msg.channel.createMessage("Kanji:", {
+        let words = msg.content.split(' ').splice(1);
+        if (words.length == 0 || words.length > 1 || words[0].length > 1) {
+            msg.channel.createMessage(":x: Please query for exactly one character.");
+            return;
+        }
+        
+        var kanji = words[0][0];
+        var kanjiFile = "0" + words[0].charCodeAt(0).toString(16) + "_anim.gif";
+
+        if (!(files.includes(kanjiFile))) {
+            msg.channel.createMessage(":x: Kanji not found or not a kanji.");
+            return;
+        }
+
+        pnfs.readFile("../kanji/" + kanjiFile)
+            .then(buffer => msg.channel.createMessage(":information_source: Here is the stroke order for 「" + kanji + "」:" , {
                 "file": buffer,
-                "name": "kanji.gif"
+                "name": kanji + ".gif"
             }))
             .catch(e => console.error(e));
     }
