057c16c2e3f13b887def0a26d8f3801ce3aa42f3
Danny <thelmuxkriovar1@gmail.com>
2017-07-29 20:51:10 +0200

    Add padding to prevent image from being cut off

--- src/commands/japanese/read.js
+++ src/commands/japanese/read.js
@@ -141,6 +141,9 @@ class Read extends Command {
                 font-size: 20pt;
                 font-weight: 700;
             }
+            #text {
+                padding: 3px;
+            }
         </style>
         <meta charset="utf8">
     </head>
