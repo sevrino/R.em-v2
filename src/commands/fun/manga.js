4aef261184de59701a66a8c483609cf9a59e0913
Taumer <taumer943@gmail.com>
2018-04-23 09:50:06 +0300

    Switch $manga to grapqhql api

--- src/commands/fun/manga.js
+++ src/commands/fun/manga.js
@@ -20,35 +20,55 @@ class MangaSearch extends Command {
         let searchQuery = msg.content.substring(msg.prefix.length + this.cmd.length + 1);
         if (!searchQuery) return await msg.channel.createMessage(this.t('generic.empty-search', {lngs: msg.lang}));
         try {
-            let authRequest = await axios.post(`https://anilist.co/api/auth/access_token`, {
-                grant_type: 'client_credentials',
-                client_id: remConfig.anilist_id,
-                client_secret: remConfig.anilist_secret
-            });
-            let accessToken = authRequest.data.access_token;
             let mangaRequest = await axios({
-                url: `https://anilist.co/api/manga/search/${encodeURI(searchQuery)}`,
-                params: {access_token: accessToken}
-            });
-            if (mangaRequest.data.error) {
-                if (mangaRequest.data.error.messages[0] === 'No Results.') {
-                    return msg.channel.createMessage(this.t('define.no-result', {lngs: msg.lang, term: searchQuery}));
+                url: `https://graphql.anilist.co/`,
+                method: `post`,
+                data: {
+                    query: `query {
+                          Page {
+                            media(search: "${searchQuery}", type: MANGA) {
+                              id
+                              title {
+                                romaji
+                                english
+                              }
+                              coverImage {
+                                large
+                              }
+                              description
+                              genres
+                              averageScore
+                              chapters
+                            }
+                          }
+                        }`
                 }
+            });
+            if (mangaRequest.data.data.Page.media.length === 0) {
+                return msg.channel.createMessage(this.t('define.no-result', {lngs: msg.lang, term: searchQuery}));
             }
-            if (mangaRequest.data.length === 1) {
-                let characters = await this.loadCharacters(mangaRequest.data[0].id, accessToken);
-                let embed = this.buildResponse(msg, mangaRequest.data[0], characters);
+            if (mangaRequest.data.data.Page.media.length === 1) {
+                let characters = await this.loadCharacters(mangaRequest.data.data.Page.media[0]);
+                let embed = this.buildResponse(msg, mangaRequest.data.data.Page.media[0], characters);
                 return msg.channel.createMessage(embed);
-            } else if (mangaRequest.data.length > 1) {
-                let pick = await new Menu(this.t('search.anime', {lngs: msg.lang}), this.t('menu.guide', {lngs: msg.lang}), mangaRequest.data.map(m => {
-                    return (m.title_english !== m.title_romaji ? `${m.title_romaji} | ${m.title_english}` : m.title_romaji)
+            } else if (mangaRequest.data.data.Page.media.length > 1) {
+                let pick = await new Menu(this.t('search.anime', {lngs: msg.lang}), this.t('menu.guide', {lngs: msg.lang}), mangaRequest.data.data.Page.media.map(a => {
+                    if (a.title.english !== a.title.romaji) {
+                        if (a.title.english === null) {
+                            return a.title.romaji
+                        } else {
+                            return `${a.title.romaji} | ${a.title.english}`
+                        }
+                    } else {
+                        return a.title.romaji
+                    }
                 }).slice(0, 10), this.t, msg);
                 if (pick === -1) {
                     return msg.channel.createMessage(this.t('generic.cancelled-command', {lngs: msg.lang}));
                 }
                 if (pick > -1) {
-                    let manga = mangaRequest.data[pick];
-                    let characters = await this.loadCharacters(manga.id, accessToken);
+                    let manga = mangaRequest.data.data.Page.media[pick];
+                    let characters = await this.loadCharacters(manga.id);
                     let embed = this.buildResponse(msg, manga, characters);
                     return msg.channel.createMessage(embed);
                 }
@@ -61,17 +81,35 @@ class MangaSearch extends Command {
         }
     }
 
-    async loadCharacters(id, token) {
+    async loadCharacters(id) {
         let characterRequest = await axios({
-            url: `https://anilist.co/api/manga/${id}/characters`,
-            params: {access_token: token}
+            url: `https://graphql.anilist.co/`,
+            method: `post`,
+            data: {
+                query: `query {
+                      Media(id: ${id}, type: MANGA) {
+                        characters(page: 1, sort: [ROLE]) {
+                          edges {
+                            role
+                            node {
+                              id
+                              name {
+                                first
+                                last
+                              }
+                            }
+                          }
+                        }
+                      }
+                    }`
+            }
         });
-        return characterRequest.data.characters;
+        return characterRequest.data.data.Media.characters.edges;
     }
 
     buildResponse(msg, data, characters) {
         let description = data.description.replace(/<br>/g, '');
-        description = description.replace(/\n|\\n/g, '');
+        description = description.replace(/\n|\r|\\n|\\r/g, '');
         description = description.replace(/&mdash;/g, '');
         description = description.replace(/&#039;/g, '');
         description = description.split('.').join('.\n\n');
@@ -80,13 +118,21 @@ class MangaSearch extends Command {
             description += '...';
         }
         let mainCharacters = characters.filter((c) => {
-            return c.role === 'Main';
+            return c.role === 'MAIN';
         });
         let characterString = mainCharacters.map(c => {
-            return `[${c.name_first}${c.name_last ? ` ${c.name_last}` : ''}](https://anilist.co/character/${c.id})`
+            return `[${c.node.name.first}${c.node.name.last ? ` ${c.node.name.last}` : ''}](https://anilist.co/character/${c.node.id})`
         });
-        characterString = characterString.join(', ');
-        let titleString = data.title_english !== data.title_romaji ? `${data.title_romaji} | ${data.title_english}` : data.title_romaji;
+        characterString = characterString.join(', ');let titleString = "";
+        if (data.title.english !== data.title.romaji) {
+            if (data.title.english === null) {
+                titleString = data.title.romaji;
+            } else {
+                titleString = `${data.title.romaji} | ${data.title.english}`;
+            }
+        } else {
+            titleString = data.title.romaji;
+        }
         return {
             embed: {
                 "title": titleString,
@@ -94,10 +140,10 @@ class MangaSearch extends Command {
                 "url": `https://anilist.co/manga/${data.id}/`,
                 "color": 0x00ADFF,
                 "footer": {
-                    "text": `⭐${this.t('anime.score', {lngs: msg.lang})}: ${data.average_score}/100`
+                    "text": `⭐${this.t('anime.score', {lngs: msg.lang})}: ${data.averageScore !== null ? data.averageScore : 0}/100`
                 },
                 "image": {
-                    "url": data.image_url_lge
+                    "url": data.coverImage.large
                 },
                 "fields": [
                     {
@@ -106,7 +152,7 @@ class MangaSearch extends Command {
                     },
                     {
                         "name": `:1234: ${this.t('manga.chapters', {lngs: msg.lang})}`,
-                        "value": `**${data.total_chapters > 0 ? data.total_chapters : `${this.t('generic.unknown', {lngs: msg.lang})}` }**`
+                        "value": `**${data.chapters > 0 ? data.chapters : `${this.t('generic.unknown', {lngs: msg.lang})}` }**`
                     },
                     {
                         "name": `:man_dancing: ${this.t('anime.characters', {lngs: msg.lang})}`,
